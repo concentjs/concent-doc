014f0485a621f928e280dbadf5ecbc0b1a6614bd
fancyzhong <fancyzhong@tencent.com>
2019-09-08 19:20:58 +0800

    change asset url

--- website/.antdsite/config.js
+++ website/.antdsite/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   title: 'concent',
   description: '一个可预测、0入侵、渐进式、高性能的增强型状态管理方案，power your react！',
-  logo: '/favicon.png',
+  logo: 'favicon.png',
   footer: 'MIT Licensed | Copyright © concentjs (author: fantasticsoul)',
-  head: [['link', { rel: 'icon', href: '/favicon.png' }]],
+  head: [['link', { rel: 'icon', href: 'favicon.png' }]],
   themeConfig: {
-    logo: '/favicon.png',
+    logo: 'favicon.png',
     themeColors: {
       'primary-color': '#0094bd'
     },
